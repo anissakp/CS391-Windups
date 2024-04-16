00af72299b19e55dadb3a972006fc6a17b3dd336
Anissa Patel <anissakp@bu.edu>
2024-04-16 14:32:08 -0400

    implemented useRewind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React from 'react';
 import styled, { css, createGlobalStyle } from 'styled-components';
-import { WindupChildren, CharWrapper } from "windups";
-
+import { WindupChildren, CharWrapper, useRewind } from "windups";
 import lightbulb from "./assets/lightbulb.png";
 
 const StyledContainer = styled.div`
-    font-family: 'Courier New', Courier, monospace; /* Typewriter font */
+    font-family: 'Courier New', Courier, monospace;
     text-align: center;
     position: absolute;
     top: 50%;
@@ -13,6 +12,21 @@ const StyledContainer = styled.div`
     transform: translate(-50%, -50%);
     width: 100%;
     font-size: 36px;
+`;
+
+const StyledButton = styled.button`
+    position: fixed;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    padding: 10px 20px;
+    font-size: 18px;
+    cursor: pointer;
+    background-color: #f0f0f0;
+    border: none;
+    border-radius: 5px;
+    margin-top: 10px;
+    color: black;
 `;
 
 const animationStyles = css`
@@ -27,7 +41,10 @@ const StyledSpan = styled.span`
 
 const GlobalStyle = createGlobalStyle`
     body {
-        background-color: black; /* Set the background color to white */
+        background-color: black;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
     }
 
     @keyframes fadeIn {
@@ -40,32 +57,34 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
-function TextAnimation() {
-    const [animationKey, setAnimationKey] = useState(0);
+const RewindButton = () => {
+    const rewind = useRewind();
+    return <StyledButton onClick={rewind}>Rewind</StyledButton>;
+};
 
-    const rewindAnimation = () => {
-        setAnimationKey(prevKey => prevKey + 1);  // Change the key to retrigger the animation
-    };
+function TextAnimation() {
+    const text = "Welcome to our S&T Presentation!";
+    const byLine = "By Anissa, Ana, Ashley, and Gary";
 
     return (
         <>
             <GlobalStyle />
             <StyledContainer>
-                <WindupChildren key={animationKey} onFinished={() => console.log("Animation Finished")}>
-                    {"Welcome to our S&T Presentation!".split("").map((char, index) => (
-                        <CharWrapper key={index} element={props => <StyledSpan {...props} char={char} color={index < 15 ? "black" : "pink"}/>}>
+                <WindupChildren>
+                    {text.split("").map((char, index) => (
+                        <CharWrapper key={index} element={props => <StyledSpan {...props} char={char} color={index < 15 ? "white" : "pink"} jump={index >= 15 && index < 33}/>}>
                             {char}
                         </CharWrapper>
                     ))}
                     <br />
-                    {"By Anissa, Ana, Ashley, and Gary".split("").map((char, index) => (
-                        <CharWrapper key={index + 100} element={props => <StyledSpan {...props} char={char} color="pink" />}>
+                    {byLine.split("").map((char, index) => (
+                        <CharWrapper key={index + 100} element={props => <StyledSpan {...props} char={char} color="white" />}>
                             {char}
                         </CharWrapper>
                     ))}
-                    <img src={lightbulb} alt="Lightbulb" style={{ display: "block", margin: "15px auto 0", width: "75px", height: "75px" }} />
+                    <RewindButton />
                 </WindupChildren>
-                <button onClick={rewindAnimation}>Rewind</button>
+                <img src={lightbulb} alt="Lightbulb" style={{ display: "block", margin: "15px auto 0", width: "75px", height: "75px" }} />
             </StyledContainer>
         </>
     );
